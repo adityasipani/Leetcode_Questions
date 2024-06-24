52bbb2e5544e1a3f933c2fab72e495da0ecc58e4
Aditya Sipani <asipani969@gmail.com>
2024-06-24 12:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.38%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+void findSubsets(int ind,vector<vector<int>>&ans,vector<int>&ds,vector<int>& nums){
+     int n=nums.size();
+    ans.push_back(ds);
+    //base case bhi cover krlega 
+    for(int i=ind;i<n;i++){
+        if(i!=ind &&nums[i]==nums[i-1])continue;
+        ds.push_back(nums[i]);
+        findSubsets(i+1,ans,ds,nums);
+        ds.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+       
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+
+        findSubsets(0,ans,ds,nums);
+        return ans;
+        
+    }
+};
