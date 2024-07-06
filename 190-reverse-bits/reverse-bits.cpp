1f25fda3fba921dcd05e14a480626d2457d9b428
Aditya Sipani <asipani969@gmail.com>
2024-07-06 20:11:55 +0530

    Time: 4 ms (36.76%) | Memory: 7 MB (92.39%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int result=0;
+        int lsb=(n&1);
+        for(int i=0;i<32;i++){
+            int lsb=(n&1);
+        int reverselsb= lsb<<(31-i);
+        result=result | reverselsb;
+          n>>=1;
+        
+    }
+     return result;
+}};
