b0e42b57c09fe7e1730489fd6f85ce8994f659cd
Aditya Sipani <asipani969@gmail.com>
2024-07-07 21:47:03 +0530

    Time: 23 ms (19.79%) | Memory: 27.8 MB (58.87%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ bool ans=false;
+
+ bool match(TreeNode* root, TreeNode* subroot){
+    if(root!=NULL &&subroot!=NULL){
+        bool a =match(root->left,subroot->left);
+        bool b=match(root->right,subroot->right);
+        if(root->val==subroot->val && a &&b)
+            return true;
+            else
+             return false;}
+    
+        else if(root==NULL && subroot==NULL){
+            return true;
+
+        }
+        else {
+            return false;
+        }
+    }
+ 
+
+   void inorder(TreeNode* root, TreeNode* subroot){
+    if(root!=NULL){
+        inorder(root->left,subroot);   //inorder left,root,right
+
+        bool x=match(root,subroot);
+        if(x){
+            ans=x;
+        }
+        inorder(root->right,subroot);
+    }
+
+
+   }
+    bool isSubtree(TreeNode* root, TreeNode* subroot) {
+        inorder(root,subroot);
+        return ans;
+        
+    }
+};
