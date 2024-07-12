cd3338f3fb40f39a6d091a06e1411f706242d0ff
Aditya Sipani <asipani969@gmail.com>
2024-07-12 13:16:31 +0530

    Time: 14 ms (95.48%) | Memory: 19.5 MB (20.73%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,38 @@
 class Solution {
-private:
-    // DFS traversal function
-    void dfs(int node, vector<int> adjLs[], vector<int>& vis) {
-        // Mark the node as visited
-        vis[node] = 1;
-        for (auto it : adjLs[node]) {
-            if (!vis[it]) {
-                dfs(it, adjLs, vis);
+    private:
+
+    void dfs(int node , vector<int>adj[],vector<int> &vis){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,vis);
             }
         }
     }
-
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> adjLs[n]; // Use n to accommodate 0-based indexing
 
-        // Convert adjacency matrix to adjacency list
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                // Self nodes are not considered
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j); // No adjustment needed for 0-based indexing
-                    adjLs[j].push_back(i); // No adjustment needed for 0-based indexing
-                }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+         int n=isConnected.size();
+       
+        vector<int>adj[n+1];
+        for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1 && i!=j){
+                adj[i+1].push_back(j+1);
+                adj[i+1].push_back(j+1);
             }
         }
 
-        vector<int> vis(n, 0); // Use n to accommodate 0-based indexing
-        int cnt = 0;
-        for (int i = 0; i < n; ++i) { // Start from 0 for 0-based indexing
-            // If the node is not visited
-            if (!vis[i]) {
-                // Counter to count the number of provinces
+        }
+        vector<int>vis(n+1,0);
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(!vis[i]){
                 cnt++;
-                dfs(i, adjLs, vis);
+                dfs(i,adj,vis);
             }
         }
         return cnt;
+        
     }
 };
