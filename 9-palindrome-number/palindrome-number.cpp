c8c5e74f74f3d2807c488d21fcc704a58c19659e
Aditya Sipani <asipani969@gmail.com>
2024-07-19 21:06:15 +0530

    Time: 11 ms (40.53%) | Memory: 8.6 MB (27.92%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long dup=x;
+        long long revNum=0;
+
+        while(x>0){
+          int ld=x%10;
+
+          revNum=(revNum*10+ld);
+
+          x=x/10;
+
+        }
+        if(dup==revNum) return true;
+        else{
+            return false;
+        }
+
+        
+    }
+};
