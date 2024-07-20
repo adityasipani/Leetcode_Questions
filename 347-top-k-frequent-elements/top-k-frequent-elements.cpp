40ad5ad42bc68f00359a64710893245d34dc2dbe
Aditya Sipani <asipani969@gmail.com>
2024-07-20 17:59:40 +0530

    Time: 10 ms (75.87%) | Memory: 17.5 MB (23.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // Create an unordered map to store the count of each number
-        unordered_map<int, int> countMap;
-        for (int& num : nums)
-            countMap[num]++;
-        vector<int> result;
-        // Create a min-heap to store the top k frequent elements
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        for (const auto& entry : countMap) {
-            // Insert the frequency and corresponding number into the min-heap
-            minHeap.emplace(entry.second, entry.first);
-            // If the size of the min-heap exceeds k, remove the element with the smallest frequency
-            if (minHeap.size() > k)
+
+        unordered_map<int,int>map;
+
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+
+        vector<int>res;
+
+        //min-heap create
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minHeap;
+
+        for(auto i=map.begin();i!=map.end();i++){
+            minHeap.push({i->second ,i->first});
+
+            if(minHeap.size()>k){
                 minHeap.pop();
+            }
         }
-        while (!minHeap.empty()) {
-            // Extract the number from the top of the min-heap and add it to the result vector
-            result.push_back(minHeap.top().second);
+
+
+        while(!minHeap.empty()){
+            res.push_back(minHeap.top().second);
             minHeap.pop();
+
         }
-        return result;
+        return res;
+        
     }
 };
