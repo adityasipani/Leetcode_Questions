6442a83841a425df0b2e677c3289ca4c41c7f33b
Aditya Sipani <asipani969@gmail.com>
2024-07-23 00:19:38 +0530

    Time: 19 ms (94.62%) | Memory: 25.1 MB (32.12%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+
+        for(auto x:strs){
+            string word=x;
+            sort(word.begin(),word.end());
+            mp[word].push_back(x);
+        }
+
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
