a19900963707883ccea0e7d0ce47812f37710549
Aditya Sipani <asipani969@gmail.com>
2024-07-23 01:26:50 +0530

    Time: 35 ms (27.24%) | Memory: 26.2 MB (18.97%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        vector<vector<string>>ans;
-
-        for(auto x:strs){
-            string word=x;
-            sort(word.begin(),word.end());
-            mp[word].push_back(x);
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            mp[strSort(s)].push_back(s);
         }
-
-        for(auto it:mp){
-            ans.push_back(it.second);
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
         }
-        return ans;
-        
+        return anagrams;
+    }
+private:
+    string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int i = 0; i < 26; i++) {
+            // Manually append the character 'i' + 'a' counter[i] times to the string t
+            for (int j = 0; j < counter[i]; j++) {
+                t += (i + 'a');
+            }
+        }
+        return t;
     }
 };
