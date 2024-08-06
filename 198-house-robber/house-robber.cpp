e0be54d7e0441a8ae82fa66f0653f1524373ad16
Aditya Sipani <asipani969@gmail.com>
2024-08-06 21:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.73%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,24 +13,47 @@ public:
     //     return dp[ind]= max(pick,notpick);
     // }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,0);
 
-        dp[0]=nums[0];
-        int neg=0;
+        int n=nums.size();
+
+        int prev=nums[0];
+        int prev2=0;
 
         for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1){
-               
-                take+=dp[i-2];
+                take+=prev2;
             }
-            int nottake=0+dp[i-1];
 
-            dp[i]=max(take,nottake);
-           
+            int nottake=0+prev;
+
+            int curi=max(take,nottake);
+            prev2=prev;
+            prev=curi;
         }
-        return dp[n-1];
+        return prev;
+
+
+
+
+        // int n=nums.size();
+        // vector<int>dp(n,0);
+
+        // dp[0]=nums[0];
+        // int neg=0;
+
+        // for(int i=1;i<n;i++){
+        //     int take=nums[i];
+        //     if(i>1){
+               
+        //         take+=dp[i-2];
+        //     }
+        //     int nottake=0+dp[i-1];
+
+        //     dp[i]=max(take,nottake);
+           
+        // }
+        // return dp[n-1];
       
 
         
