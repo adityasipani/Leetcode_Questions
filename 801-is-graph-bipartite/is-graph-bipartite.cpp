627d57cc400fe7cec5d64792114bbb826aa96a51
Aditya Sipani <asipani969@gmail.com>
2024-12-27 01:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (45.61%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,38 +1,22 @@
 class Solution {
     private:
-    bool check(int node,vector<vector<int>>& graph,vector<int>&color){
-        queue<int>q;
-        q.push(node);
-        color[node]=0;
+    bool check(int start,int col,vector<vector<int>>& graph,vector<int>&color ){
+        color[start]=col;
 
-        while(!q.empty()){
-            int bro=q.front();
-            q.pop();
-
-            for(auto it:graph[bro]){
-                if(color[it]==-1){
-                    color[it]=!color[bro];
-                    q.push(it);
-
-                }
-                else if(color[it]==color[bro]){
-                    return false;
-                }
-            }
+        for(auto it:graph[start]){
+            if(color[it]==-1){
+               if (check(it,!col,graph,color)==false)return false; }
+            else if(color[it]==color[start])return false;
         }
         return true;
-
-    
-
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>color(n,-1);
-
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                if(check(i,graph,color)==false)return false;
+                if(check(i,0,graph,color)==false)return false;
             }
         }
         return true;
