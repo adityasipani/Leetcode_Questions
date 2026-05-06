775e502c19e89004c02ac93f5d2a06cdc71bf33c
Aditya Sipani <asipani969@gmail.com>
2026-05-06 12:39:05 +0530

    Time: 0 ms (100%), Space: 16.3 MB (62.96%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,36 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-          
-        if(!head || !head->next || k==0)return head;
-        
-        ListNode*cur=head;
-        int len=1;
-        
-        while(cur->next && ++len){
-            cur=cur->next;
+        if (!head) return head;
+
+        int length = 1;
+        ListNode* dummy = head;
+
+        while (dummy->next) {
+            dummy = dummy->next;
+            length++;
         }
-        cur->next=head; //circular
-        
-        k=k%len;
-        k=len-k;
-        
-        while(k--)
-            cur=cur->next;
-        
-        head=cur->next;
-        cur->next=NULL;
-        
-        return head;
-    }}
-;
+
+        int position = k % length;
+        if (position == 0) return head;
+
+        ListNode* current = head;
+        for (int i = 0; i < length - position - 1; ++i) {
+            current = current->next;
+        }
+
+        ListNode* newHead = current->next;
+        current->next = nullptr;
+        dummy->next = head;
+
+        return newHead;        
+    }
+};
